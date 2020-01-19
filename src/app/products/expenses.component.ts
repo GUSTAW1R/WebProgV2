b488f710ed7543af83c064ab3810b1d6625b2a4a
GUSTAW1R <yamantaw@mail.ru>
2020-01-19 14:53:11 +0400

    Laboratornaya_10

--- src/app/products/expenses.component.ts
+++ src/app/products/expenses.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms'
 import { ExpensService } from 'src/app/products/shared/expens.service'
 import { ToastrService } from 'ngx-toastr';
 import { ExpensListComponent } from './expens-list/expens-list.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-expenses',
@@ -13,7 +15,7 @@ export class ExpensesComponent implements OnInit {
 
   @ViewChild(ExpensListComponent, {static: false}) list: ExpensListComponent;
 
-  constructor(private expensService: ExpensService, private toastr: ToastrService) {
+  constructor(private expensService: ExpensService, private toastr: ToastrService, private http: HttpClient) {
     this.ws.onopen = () => {
           this.setStatus('ONLINE');
        this.ws.onmessage = (response) => {
@@ -24,6 +26,15 @@ export class ExpensesComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.toastr.success("до запроса")
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    const data: any = {
+      'url': 'ws://localhost:3000'
+    };
+    this.http.post("http://localhost:3100/subscribe", <JSON>data, httpOptions).subscribe(data => {
+      console.log(data);
+    });
+    console.log("после запроса2")
     this.resetForm();
   }
   private sub = document.getElementById('submit');
@@ -39,7 +50,7 @@ export class ExpensesComponent implements OnInit {
     console.log("I'm is Admin and i send message!");
     var te = new TextEncoder();
     this.ws.send(this.expensService.selectedExpens.Name); 
-    //this.ws.send('isUpgrade');
+    this.ws.send('isUpgrade');
   }
   resetForm(form?: NgForm) {
     if (form != null)
